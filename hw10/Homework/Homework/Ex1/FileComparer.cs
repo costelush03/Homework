09e3d7ac1e14cfd0cf54cbe2f374a4f51ddb4209
Constantin <konstantin.danilov10@gmail.com>
2026-02-08 21:03:20 +0200

    Add Folder hw10

---
+++ hw10/Homework/Homework/Ex1/FileComparer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Homework.Ex1
+{
+    public static class FileComparer
+    {
+        public static void Run()
+        {
+            string file1 = "file1.txt";
+            string file2 = "file2.txt";
+
+            if (!File.Exists(file1))
+                File.WriteAllText(file1, "Text in file 1");
+
+            if (!File.Exists(file2))
+                File.WriteAllText(file2, "Text in file 2");
+
+            string content1 = File.ReadAllText(file1);
+            string content2 = File.ReadAllText(file2);
+
+            if (content1 == content2)
+                Console.WriteLine("The files are identical.");
+            else
+                Console.WriteLine("The files are different.");
+        }
+    }
+}
+
+
