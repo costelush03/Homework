faef633ddb818386c37ab5d03476fe3ea5126566
Constantin <konstantin.danilov10@gmail.com>
2026-02-25 23:38:29 +0200

    Add hw_21

---
+++ hw_21/hw_21/Program.cs
@@ -0,0 +1,7 @@
+﻿using hw_21.Services;
+
+var intOps = new MathOperations<int>();
+Console.WriteLine(intOps.Add(5, 7));        // 12
+
+var dblOps = new MathOperations<double>();
+Console.WriteLine(dblOps.Divide(20, 4));    // 5
---
+++ hw_21/hw_21/Services/MathOperations.cs
@@ -0,0 +1,21 @@
+﻿using System.Numerics;
+
+namespace hw_21.Services
+{
+    public class MathOperations<T> where T : INumber<T>
+    {
+        public T Add(T a, T b) => a + b;
+
+        public T Subtract(T a, T b) => a - b;
+
+        public T Multiply(T a, T b) => a * b;
+
+        public T Divide(T a, T b)
+        {
+            if (b == T.Zero)
+                throw new ArgumentException("Nu poți împărți la zero.");
+
+            return a / b;
+        }
+    }
+}
